94fa81fc87454b5ab3897acb7163d1f1ce14e278
Paulo Marinas <paulopmx@gmail.com>
2011-07-13 22:41:24 +0800

    Add theme toggling on template

---
+++ js/flexigrid-2.0.min.js
@@ -0,0 +1,8 @@
+/*
+ * Flexigrid for jQuery - New Wave Grid
+ *
+ * Copyright (c) 2011 Paulo P. Marinas (www.flexigrid.info)
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ *
+*/
