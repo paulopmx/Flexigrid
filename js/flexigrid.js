8991d3d81dff6193125f5eb5bbcee18e1e2f862f
Cornelius Kölbel <cornelius.koelbel@lsexperts.de>
2012-07-12 21:40:45 +0200

    fix toggle singleSelect

--- js/flexigrid.js
+++ js/flexigrid.js
@@ -688,7 +688,6 @@
 						$(this).toggleClass('trSelected');
 						if (p.singleSelect && ! g.multisel ) {
 							$(this).siblings().removeClass('trSelected');
-							$(this).addClass('trSelected');
 						}
 					}).mousedown(function (e) {
 						if (e.shiftKey) {
