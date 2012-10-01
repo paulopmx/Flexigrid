5a7ec5cafdc33011ce9cf49d601258bb04fb30ad
Mario Basic <mario.basic@damex.hr>
2012-10-01 23:39:47 +0200

    Updated post-xml.php to post-xml-mysqli.xml.
    Connection and queries to the database are made with mysqli. It's much
    safer and works with latest apache and php versions. I had problems
    using the old php file so i made this one.

---
+++ demo/post-xml-mysqli.php
@@ -0,0 +1,82 @@
+<?php
+$page = isset($_POST['page']) ? $_POST['page'] : 1;
+$rp = isset($_POST['rp']) ? $_POST['rp'] : 10;
+$sortname = isset($_POST['sortname']) ? $_POST['sortname'] : 'name';
+$sortorder = isset($_POST['sortorder']) ? $_POST['sortorder'] : 'desc';
+$query = isset($_POST['query']) ? $_POST['query'] : false;
+$qtype = isset($_POST['qtype']) ? $_POST['qtype'] : false;
+
+$db['default']['hostname'] = "localhost";
+$db['default']['username'] = '';
+$db['default']['password'] = "";
+$db['default']['database'] = "";
+
+$db['live']['hostname'] = 'localhost';
+$db['live']['username'] = '';
+$db['live']['password'] = '';
+$db['live']['database'] = '';
+
+$active_group = 'default';
+
+$base_url = "http://".$_SERVER['HTTP_HOST'];
+$base_url .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+
+$conn = new mysqli($db[$active_group]['hostname'],$db[$active_group]['username'],$db[$active_group]['password'],$db[$active_group]['database']);
+$conn->set_charset("utf8");
+if ($conn->connect_errno) {
+	die("Failed to connect to MySQL: (" . $conn->connect_errno . ") " . $conn->connect_error);
+}
+
+function countRec($fname,$tname,$conn) {
+	$sql = $conn->prepare("SELECT count(?) FROM $tname ");
+	$sql->bind_param("s", $fname);
+	$sql->execute();
+	$sql->store_result();
+
+	return $sql->num_rows;
+}
+
+$page = $_POST['page'];
+$rp = $_POST['rp'];
+$sortname = $_POST['sortname'];
+$sortorder = $_POST['sortorder'];
+
+if (!$sortname) $sortname = 'name';
+if (!$sortorder) $sortorder = 'desc';
+if (!$page) $page = 1;
+if (!$rp) $rp = 10;
+
+$start = (($page-1) * $rp);
+
+if ($query){
+	$sql = $conn->prepare("SELECT id_country, name FROM country WHERE $qtype LIKE ? ORDER BY $sortname $sortorder LIMIT ?, ?");
+	$query = "%".$query."%";
+	$sql->bind_param("sii", $query, $start, $rp);
+	
+} else {
+	$sql = $conn->prepare("SELECT id_country, name FROM country ORDER BY $sortname $sortorder LIMIT ?, ?");
+	$sql->bind_param("ii", $start, $rp);
+
+}
+
+$sql->execute();
+$sql->store_result();
+$sql->bind_result($id_country, $name);
+
+$total = countRec('id_country','country',$conn);
+
+header("Content-type: text/xml");
+$xml = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n";
+$xml .= "<rows>";
+$xml .= "<page>$page</page>";
+$xml .= "<total>$total</total>";
+
+while($sql->fetch()){	
+	$xml .= "<row id='".$id_country."'>";
+	$xml .= "<cell><![CDATA[".$id_country."]]></cell>";
+	$xml .= "<cell><![CDATA[".$name."]]></cell>";
+	$xml .= "</row>";
+}
+
+$xml .= "</rows>";
+echo $xml;
